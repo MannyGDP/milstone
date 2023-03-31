16a93776ff4d0420ced3689623d12389b38df0c9
Manny Delgado <mannygdp@gmail.com>
2023-03-30 20:55:11 -0700

    made changes to JS and CSS files

--- code.js
+++ code.js
@@ -1,7 +1,23 @@
 // Define the game logic
-let currentPlayer = 'X';
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer =X_TEXT
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+  boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked(e) {
+  console.log(e.target)
+}
+
+startGame()
+//#
 function checkWin() {
   // Check all possible winning combinations
 }
